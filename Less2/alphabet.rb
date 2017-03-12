31e860f41cec4cbba6493e8bd43ee21dda49e6bf
avoca8o <uktras@gmail.com>
2017-03-12 15:28:33 +0300

    alphabet fix 2

--- Less2/alphabet.rb
+++ Less2/alphabet.rb
@@ -1,9 +1,7 @@
 words = {}
-x = 0
-
 vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 
-vowels.each.with_index(1) do |letter, index|
+('a' .. 'z').each.with_index(1) do |letter, index|
   words[letter] = index if vowels.include?(letter)
 end
 
